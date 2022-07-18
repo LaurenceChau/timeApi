08de9ad4eec490b0fed7df2b9fcf2e49cd2b7ffa
laurencechau <laurence_chau@hotmail.com>
2022-07-18 13:34:20 +0800

    refactor: shorten getTime func

--- main.go
+++ main.go
@@ -13,31 +13,32 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/api/time", getTime)
-
+	log.Print("Application started. Listening for requests.")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
 
 func getTime(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
+	timezones := r.URL.Query().Get("tz")
 
-	tzMap := make(map[string]string)
-	for _, tz := range strings.Split(q.Get("tz"), ",") {
+	resp := make(map[string]string)
+	for _, tz := range strings.Split(timezones, ",") {
 		loc, err := time.LoadLocation(tz)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w, "Invalid timezone")
+			log.Print("Invalid timezone")
 			return
 		}
 
 		if tz == "" {
 			tz = "current_time"
 		}
-		log.Println(tz)
-		tzMap[tz] = time.Now().In(loc).String()
+		log.Printf("timezone: %s", tz)
+		resp[tz] = time.Now().In(loc).String()
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tzMap)
+	w.Header().Add("Content-type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+
 }
